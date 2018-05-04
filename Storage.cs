3fcc5289aeba0b0ffd84b263e458931f59a7a530
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-05-04 14:49:19 +0100

    Added overloads for storing and retrieving JSON-serializable objects

--- Storage.cs
+++ Storage.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) 2018 cloudcrate solutions UG (haftungsbeschraenkt)
 
+using Microsoft.AspNetCore.Blazor;
 using Microsoft.AspNetCore.Blazor.Browser.Interop;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -27,6 +28,12 @@ namespace Cloudcrate.AspNetCore.Blazor.Browser.Storage
             return RegisteredFunction.Invoke<string>($"{_fullTypeName}.GetItem", key);
         }
 
+        public T GetItem<T>(string key)
+        {
+            var json = GetItem(key);
+            return string.IsNullOrEmpty(json) ? default(T) : JsonUtil.Deserialize<T>(json);
+        }
+
         public string Key(int index)
         {
             return RegisteredFunction.Invoke<string>($"{_fullTypeName}.Key", index);
@@ -48,6 +55,11 @@ namespace Cloudcrate.AspNetCore.Blazor.Browser.Storage
         public void SetItem(string key, string data)
         {
             RegisteredFunction.Invoke<object>($"{_fullTypeName}.SetItem", key, data);
+        }
+
+        public void SetItem(string key, object data)
+        {
+            SetItem(key, JsonUtil.Serialize(data));
         }
 
         public string this[string key]
